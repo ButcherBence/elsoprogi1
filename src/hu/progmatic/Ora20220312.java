ecb9de5302a540548685244c5b77991683234d9d
ButcherBence <bence.butcher@gmail.com>
2022-03-12 14:26:02 +0100

    Commit :D

--- src/hu/progmatic/Ora20220312.java
+++ src/hu/progmatic/Ora20220312.java
@@ -1,8 +1,10 @@
 package hu.progmatic;
 
+import java.util.Scanner;
+
 public class Ora20220312 {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
         
     }
 }
